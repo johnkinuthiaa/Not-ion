bca99bdc69956c06ba3e30b0760743df3adabf03
johnkinuthiaa <kinuthiajohn122@gmail.com>
2025-04-05 15:16:02 +0300

    initial commit

---
+++ src/main/java/com/slippery/notion/NotIonApplication.java
@@ -0,0 +1,13 @@
+package com.slippery.notion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotIonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotIonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/slippery/notion/NotIonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slippery.notion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotIonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
